84fc7c43311afe5e8d1aea7038179424383b8084
Gianguido Sorà <g.sora4@gmail.com>
2016-03-28 23:39:17 +0200

    daemonize ;)

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"flag"
 	"io/ioutil"
 	"log"
 	"os"
 
+	"github.com/VividCortex/godaemon"
 	"github.com/gsora/slaidsciou/config"
 	"github.com/gsora/slaidsciou/cycles"
 )
@@ -12,6 +14,16 @@ import (
 var doThis func(imgs []os.FileInfo, conf config.SlaidsciouConfig)
 
 func main() {
+	// Software options
+	foreground := flag.Bool("foreground", false, "don't fork in background, useful for debug purposes")
+	flag.Parse()
+
+	// Daemonize, or foreground :)
+	if *foreground {
+		godaemon.MakeDaemon(&godaemon.DaemonAttr{})
+	}
+
+	godaemon.MakeDaemon(&godaemon.DaemonAttr{})
 	conf, err := config.LoadConfig()
 
 	if err != nil {
