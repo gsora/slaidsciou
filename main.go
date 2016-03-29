5de0367495424cfa9fabb8ac995d6c81e3d8afe0
Gianguido Sorà <g.sora4@gmail.com>
2016-03-29 22:55:25 +0200

    don't spawn yourself if you're already alive

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
+	"strconv"
+	"strings"
 
 	"github.com/VividCortex/godaemon"
 	"github.com/gsora/slaidsciou/config"
@@ -17,6 +21,19 @@ func main() {
 	// Software options
 	foreground := flag.Bool("foreground", false, "don't fork in background, useful for debug purposes")
 	flag.Parse()
+
+	// First things first: check for any other living instances of yourself.
+	// If any, exit.
+	cmd := exec.Command("pgrep", "slaidsciou")
+
+	myPid := strconv.Itoa(os.Getpid())
+	out, _ := cmd.CombinedOutput()
+	k := strings.Split(string(out), "\n")
+
+	if myPid != k[0] {
+		fmt.Println("Slaidsciou already running, exiting...")
+		return
+	}
 
 	// Daemonize, or foreground :)
 	if *foreground {
